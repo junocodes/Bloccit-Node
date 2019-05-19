f04000b7dfa18d339242b9bfa8c38497d7821ca5
Justin Juno <hello@juno.codes>
2019-05-19 14:29:54 -0500

    Express CP-A Complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,12 +1,27 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marcoBase = `${base}marco`;
 
 describe("routes : static", () => {
   describe("GET /", () => {
     it("should return status code 200", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+  describe("GET /", () => {
+    it("should return status code 200", done => {
+      request.get(marcoBase, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+    it("should return string 'polo'", done => {
+      request.get(marcoBase, (err, res, body) => {
+        expect(body).toContain("polo");
         done();
       });
     });
